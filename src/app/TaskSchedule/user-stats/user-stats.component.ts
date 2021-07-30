a91e78f9a59a296a91628762a0fa678f5daedc1e
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2021-07-30 12:08:29 +0200

    Update student stat page

--- src/app/TaskSchedule/user-stats/user-stats.component.ts
+++ src/app/TaskSchedule/user-stats/user-stats.component.ts
@@ -58,12 +58,4 @@ export class UserStatsComponent implements OnInit {
     return '';
   }
 
-  GetCourseName(item: any): string {
-    return Object.keys(item)[0];
-  }
-
-  GetCourseTime(item: any): any {
-    return item[Object.keys(item)[0]];
-  }
-
 }
